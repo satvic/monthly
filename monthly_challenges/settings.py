826aef82bfd9fc7610bdaa4f738cf1ab59aa890e
satvicsri <srivastava.satvik@gmail.com>
2021-08-12 00:18:56 +0530

    Day 2: Hidden Secret Key

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$&sd1dfd&0w%d*fdvzg6j-9rl5s#_7l7(m@^nze(4%)@t$ah2g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
